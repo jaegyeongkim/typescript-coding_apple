d9e89af33b936f09e229569f4f4c30156355f5a6
Jake <jaegyeong@coconutsilo.com>
2022-05-12 13:16:36 +0900

    3강 예제

---
+++ index3example.js
@@ -0,0 +1,13 @@
+// Q1
+let user = "kim";
+let age = undefined;
+let married = false;
+let 철수 = [user, age, married];
+// Q2
+let 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
---
+++ index3example.ts
@@ -0,0 +1,18 @@
+// Q1
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | undefined | number | boolean)[] = [user, age, married];
+
+// Q2
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
